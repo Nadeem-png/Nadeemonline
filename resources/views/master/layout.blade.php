9c371b508b19c4aabc9ab545081141dcba19b071
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-07-31 22:17:32 +0500

    Update layout.blade.php

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -177,7 +177,7 @@ $(document).ready(function(){
 <a href="https://github.com/Nadeem-png" class="ml-4 ">
             <i class="fab fa-github color" style="font-size: 25px"></i>
         </a>
-<img src="{{asset('image/nk2.png')}}" class="img-fluid mt-5" style="margin-left:30px;width:50%; height:150px">
+<img src="{{asset('image/nk2.png')}}" class="img-fluid mt-3" style="margin-left:30px;width:100%; height:120px">
        
         <h6 class="mt-3">&copy; 2020-<?php echo date("Y") ?> Nadeem khan - Web Developer. All Rights Reserved.</h6>
 </div>
