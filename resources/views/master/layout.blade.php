39b7657e17bf3d0024dac96ae04eb1bcb054c411
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-08-01 21:49:26 +0500

    Update layout.blade.php

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -90,7 +90,7 @@ background-image: linear-gradient( 358.4deg,  rgba(249,151,119,1) -2.1%, rgba(98
     height:100%;
     position:fixed;
     z-index:9999;
-    background:url("https://www.creditmutuel.fr/cmne/fr/banques/webservices/nswr/images/loading.gif") no-repeat center center rgba(0,0,0,0.25)
+    background:url("https://www.creditmutuel.fr/cmne/fr/banques/webservices/nswr/images/loading.gif") no-repeat center center rgba(0,0,0,1);
 }
 
             
@@ -121,22 +121,7 @@ $(document).ready(function(){
 
 
 
-<script>
 
-document.onreadystatechange = function () {
-  var state = document.readyState
-  if (state == 'interactive') {
-       document.getElementById('contents').style.visibility="hidden";
-  } else if (state == 'complete') {
-      setTimeout(function(){
-         document.getElementById('interactive');
-         document.getElementById('load').style.visibility="hidden";
-         document.getElementById('contents').style.visibility="visible";
-      },1000);
-  }
-}
-
-</script>
 </head>
 <body >
 <div id="load"></div>
@@ -213,6 +198,22 @@ document.onreadystatechange = function () {
 </div>
 <script>
   AOS.init();
+</script>
+<script>
+
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}
+
 </script>
 </body>
 </html>
